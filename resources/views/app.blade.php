a6a8b922bf9d76d1bbc01206093cd122200128f6
EvilDemo127 <maungzawshein20@gmail.com>
2026-07-10 17:07:02 +0700

    Fix Vite manifest missing app.css error

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
 
         @routes
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite([ 'resources/js/app.js'])
 
   
         <x-inertia::head />
